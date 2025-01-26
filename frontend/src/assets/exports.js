0867611708c916322eabd24b206a7fa0e70800d3
Aditya Shibu <as2397@hw.ac.uk>
2025-01-26 22:57:28 +0400

    Initialize frontend project with Next.js, Tailwind CSS, and essential configurations

--- frontend/src/assets/exports.js
+++
@@ -1,5 +0,0 @@
-import messages1 from "./images/messages-1.jpg";
-import messages2 from "./images/messages-2.jpg";
-import profileImg from "./images/profileImg.jpg";
-
-export { messages1, messages2, profileImg };
--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        jetBrains: ["JetbrainsBold", "monospace"],
-        jetBrainsExtraBold: ["JetbrainsExtraBold", "monospace"],
-      },
       colors: {
-        "main-light-blue-dark": "#1F99FC",
-
-        "main-dark-purple-dark": "#4C2DA8",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
