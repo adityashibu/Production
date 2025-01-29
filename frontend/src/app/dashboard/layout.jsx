ebc03b75ded3252083d84f2900d67b2c96c8f43a
Aditya Shibu <as2397@hw.ac.uk>
2025-01-29 16:52:55 +0400

    Refactor layout component and update styling; migrate globals.css to ui directory

--- frontend/src/app/dashboard/layout.jsx
+++ frontend/src/app/dashboard/layout.jsx
@@ -3,11 +3,11 @@ const { default: Sidebar } = require("../ui/dashboard/sidebar/sidebar");
 
 const layout = ({ children }) => {
   return (
-    <div>
-      <div>
+    <div className="flex">
+      <div className="sm:flex-3 md:flex-1 bg-bg-lightSoft p-[20px]">
         <Sidebar />
       </div>
-      <div>
+      <div className="sm:flex-1 md:flex-4">
         <Navbar />
         {children}
       </div>
@@ -16,3 +16,4 @@ const layout = ({ children }) => {
 };
 
 export default layout;
+
