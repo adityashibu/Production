f3a77814e602a76441c4278fe5e8040acbff5b78
Aditya Shibu <as2397@hw.ac.uk>
2025-01-29 17:37:00 +0400

    Update font paths in globals.css and rename Navbar title to 'PowerHouse'

--- frontend/src/app/ui/dashboard/navbar/navbar.jsx
+++ frontend/src/app/ui/dashboard/navbar/navbar.jsx
@@ -11,7 +11,6 @@ import Badge from "@mui/material/Badge";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
@@ -172,9 +171,14 @@ export default function Navbar() {
             variant="h6"
             noWrap
             component="div"
-            sx={{ display: { xs: "none", sm: "block" } }}
+            sx={{
+              display: {
+                xs: "none",
+                sm: "block",
+              },
+            }}
           >
-            MUI
+            PowerHouse
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
