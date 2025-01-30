b14066829f7c1d2b199534b2167b6c7272002a85
Aditya Shibu <as2397@hw.ac.uk>
2025-01-30 08:17:01 +0400

    Remove Sidebar component from dashboard layout

--- frontend/src/app/dashboard/layout.jsx
+++ frontend/src/app/dashboard/layout.jsx
@@ -1,15 +1,11 @@
 import ThemeRegistry from "../themeRegistery";
 
 const { default: Navbar } = require("../ui/dashboard/navbar/navbar");
-// const { default: Sidebar } = require("../ui/dashboard/sidebar/sidebar");
 
 const layout = ({ children }) => {
   return (
     <ThemeRegistry>
       <div>
-        {/* <div className="sm:flex-3 md:flex-1 bg-bg-lightSoft p-[20px]">
-        <Sidebar />
-      </div> */}
         <div>
           <Navbar />
           {children}
