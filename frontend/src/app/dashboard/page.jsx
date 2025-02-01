357974924ec7f18d020a1cce172d507020ddf449
Aditya Shibu <as2397@hw.ac.uk>
2025-02-01 20:27:52 +0400

    Enhance dashboard layout with new card components and improve responsiveness (Cards still in progress)

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -7,6 +7,14 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid2";
 import { Box } from "@mui/material";
 
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
+import Stack from "@mui/material/Stack";
+
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: "#fff",
   ...theme.typography.body2,
@@ -25,16 +33,102 @@ const Dashboard = () => {
       <Box height={30} />
       <Grid container spacing={2}>
         <Grid size={8}>
-          <Item>size=8</Item>
+          <Stack spacing={2} direction={{ sm: "column", md: "row" }}>
+            <Card sx={{ maxWidth: 49 + "%", height: 140 }}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              {/* <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions> */}
+            </Card>
+
+            <Card sx={{ maxWidth: 49 + "%", height: 140 }}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              {/* <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions> */}
+            </Card>
+          </Stack>
         </Grid>
+
         <Grid size={4}>
-          <Item>size=4</Item>
+          <Stack spacing={2} direction="column">
+            <Card sx={{ maxWidth: 345 }}>
+              <CardContent>
+                <Stack spacing={2} direction="row">
+                  <div className="mt-[50px] ml-[20px]">
+                    <CalendarMonthIcon />
+                  </div>
+                  <div className="pl-[10px] pr-[10px] pt-[10px] pb-[10px]">
+                    {/* <span className="">230 KwH</span>
+                    <span>Monthly Usage</span> */}
+                    <Typography
+                      sx={{ fontFamily: "JetBrains Mono", fontWeight: 700 }}
+                    >
+                      230 KwH
+                    </Typography>
+                    <Typography
+                      sx={{ fontFamily: "JetBrains Mono", fontWeight: 400 }}
+                    >
+                      Monthly Usage
+                    </Typography>
+                  </div>
+                </Stack>
+              </CardContent>
+              {/* <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions> */}
+            </Card>
+
+            <Card sx={{ maxWidth: 345 }}>
+              <CardContent></CardContent>
+              {/* <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions> */}
+            </Card>
+          </Stack>
         </Grid>
-        <Grid size={4}>
-          <Item>size=4</Item>
-        </Grid>
+
         <Grid size={8}>
-          <Item>size=8</Item>
+          <Stack spacing={2} direction="column">
+            <Card sx={{ height: 60 + "vh" }}>
+              <CardContent></CardContent>
+              {/* <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions> */}
+            </Card>
+          </Stack>
+        </Grid>
+        <Grid size={4}>
+          <Stack spacing={2} direction="column">
+            <Card sx={{ height: 60 + "vh" }}>
+              <CardContent></CardContent>
+              {/* <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions> */}
+            </Card>
+          </Stack>
         </Grid>
       </Grid>
     </div>
