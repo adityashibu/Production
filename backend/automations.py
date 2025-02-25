3be2c868c540249711e99319d711e4f59892a3cf
David <dgf2001@hw.ac.uk>
2025-02-26 00:18:30 +0400

    Automation JSON load/save functions written

--- backend/automations.py
+++ backend/automations.py
@@ -1,5 +1,21 @@
-import devices_json as dj
+import json
 import schedule
 
-
 automationFile = "automations.json"
+updates = [] # For frontend
+
+
+def loadAutomations():
+    try:
+        with open(automationFile, "r") as JSONfile:
+            return json.load(JSONfile)
+    except FileNotFoundError:
+        updates.append("Error: automations.json not found!")
+        return {"automations": []}
+    
+
+def saveAutomations(data):
+    with open(automationFile, "w") as JSONfile:
+        json.dump(data, JSONfile, indent=2)
+
+        
