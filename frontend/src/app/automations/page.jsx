25e1705719d5391e3339e368a862a3a4d49492e1
Aditya <as2397@hw.ac.uk>
2025-02-26 00:40:49 +0400

    Add button for adding automation schedules in the Automations page

--- frontend/src/app/automations/page.jsx
+++ frontend/src/app/automations/page.jsx
@@ -2,10 +2,39 @@
 import React from "react";
 import Breadcrumb from "../ui/dashboard/breadcrumbs";
 
+import { Box, Button } from "@mui/material";
+
+import AddIcon from "@mui/icons-material/Add";
+
+
 const Automations = () => {
   return (
     <div>
       <Breadcrumb />
+      <Box sx={{ paddingTop: 2 }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+            marginBottom: 3,
+          }}
+        >
+          <Button
+            variant="contained"
+            startIcon={<AddIcon />}
+            // onClick={handleAddDeviceClick}
+            sx={{
+              fontFamily: "JetBrains Mono",
+              fontWeight: 600,
+              textTransform: "none",
+              color: "white",
+            }}
+          >
+            Add Automation Schedule
+          </Button>
+        </Box>
+      </Box>
     </div>
   );
 };
